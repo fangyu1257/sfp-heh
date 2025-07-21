ea2888f230ca00f2e0100dc81dc83fa67733d3df
fy <25051467@imail.sunway.edu.my>
2025-07-21 05:55:54 +0000

    work done

---
+++ Part1/ex03.py
@@ -0,0 +1,17 @@
+# Original text
+string1 = 'MaRviN THE bOt'
+
+#convert to lowercase
+string_lower = WHAT
+
+#convert to uppercase 
+string_upper = HELLO
+
+#remove spaces
+string_nospace = BYEE
+
+# Print results
+print(string1)
+print(string_lower)
+print(string_upper)
+print(string_nospace)
